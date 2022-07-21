e061aa702b9a8bc91581d1a04626900f854995cc
davidnithil <38452904+davidnithil@users.noreply.github.com>
2022-07-21 16:09:09 +0530

    Create config.tf

---
+++ .github/workflows/config.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.14.0"
+    }
+  }
+   backend "azurerm" {
+        resource_group_name  = "tfstateRG"
+        storage_account_name = "tfstatestoregithubaction"
+        container_name       = "tfstatecontainer"
+        key                  = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
