075c2c23ff4e5f79930ec9ecdefb9042f235f3a8
david kumar <rockynithil@gmail.com>
2026-02-16 21:43:53 +0530

    env

--- config.tf
+++ config.tf
@@ -15,9 +15,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-  subscription_id = "c5f5a19b-5362-4e10-90c4-887f964f902b"
-  client_id       = "67f29aa5-ed22-40bc-8c51-15d88d0c7bc0"
-  client_secret   = "~LQ8Q~ldfZgmwTrUGrh6ozkh_qyYQNfZkhE7ddia"
-  tenant_id       = "24fe11b5-e609-4e3a-83bd-149c3f1451cb"
 }
