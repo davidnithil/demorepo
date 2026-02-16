03e447f2b448411bfd716ec1e3d06db12aa4e39f
david kumar <rockynithil@gmail.com>
2026-02-16 23:14:47 +0530

    valid SUB

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "c5f5a19b-5362-4e10-90c4-887f964f902b"
+  tenant_id = "24fe11b5-e609-4e3a-83bd-149c3f1451cb"
 }
 
 resource "azurerm_resource_group" "name" {
