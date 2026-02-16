06e5771739069fd3959d16f7abca59da7f2dbc39
david kumar <rockynithil@gmail.com>
2026-02-16 22:39:47 +0530

    one file

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "name" {
   name     = "RG1"
